68ccc2c9081004816b60698c25b73f3d0b1e8837
noczero <zeroonetm@gmail.com>
2016-04-16 20:20:11 +0700

    APTRG v1

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
- 
+  var path = require('path');
 var express = require('express');
 var app = express();
 var  server = require('http').createServer(app);
 var  io = require('socket.io').listen(server);
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/client.html')
+    res.sendFile(__dirname + '/public/index.html')
   });
 
 var numConexion = 0;
